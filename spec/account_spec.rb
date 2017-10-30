985b61eb53dd943bfe1c5506082dc6977bd3332a
Marcus Rands <mrands@hotmail.co.uk>
2017-10-30 14:38:18 +0000

    Added an array to store a list of transactions. Test passing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,8 @@ describe Account do
   it 'is instantiated with a balance of 0' do
     expect(account.balance).to eq 0
   end
+
+  it 'is instantiated with an empty transactions array' do
+    expect(account.transactions).to eq []
+  end
 end
