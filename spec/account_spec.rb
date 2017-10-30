882b104874f12e7ac125d2524ec202bfd2e12935
Marcus Rands <mrands@hotmail.co.uk>
2017-10-30 14:32:48 +0000

    Initial commit. File structure in place and rspec initialised

---
+++ spec/account_spec.rb
@@ -0,0 +1,9 @@
+require './lib/account.rb'
+
+describe Account do
+  subject(:account) {described_class.new}
+
+  it 'is instantiated with a balance of 0' do
+    expect(account.balance).to eq 0
+  end
+end
