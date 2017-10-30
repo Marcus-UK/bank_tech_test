b1ca76167ac9fc0b1381b366f24112f07c085dc5
Marcus Rands <mrands@hotmail.co.uk>
2017-10-30 16:01:56 +0000

    updated transactions payment method to allow the date of a transaction to be stored. Test passing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Transaction
 
 attr_reader :transactions
@@ -6,11 +8,18 @@ attr_reader :transactions
     @transactions = []
   end
 
-  def payment(amount)
-    @payment = {amount: amount}
+  def payment(amount, date = transaction_date)
+    @payment = {date: date}
+    @amount = {amount: amount}
+    @payment.merge!(@amount)
     @transactions << @payment
   end
 
+private
 
+def transaction_date
+  @date = Time.now
+  @date.strftime("%d/%m/%Y")
+end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,21 +2,26 @@ require './lib/transaction.rb'
 
 describe Transaction do
   subject(:transaction) {described_class.new}
-
+  let(:date) { '25/12/2017'}
   it 'is instantiated with an empty array for transactions' do
     expect(transaction.transactions).to eq []
   end
 
 
-  describe '# deposit_funds' do
+  describe '# payment' do
 
-    it 'adds a hash to the transactions array' do
+    it 'adds a payment amount to a hash to be stored in transactions' do
       transaction.payment(10)
-      expect(transaction.transactions[0]).to eq ({:amount => 10})
+      expect(transaction.transactions[0][:amount]).to eq 10
+    end
+
+    it 'records the date of the transaction being stored in transactions' do
+      transaction.payment(10, date)
+      expect(transaction.transactions[0][:date]).to eq date
     end
   end
 
 
 
-  
+
 end
