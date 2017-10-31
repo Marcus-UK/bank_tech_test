2a6eec339de2ce44cf70f6061cbf6facc86d5e8d
Marcus Rands <mrands@hotmail.co.uk>
2017-10-31 09:53:51 +0000

    Refactored tests to reflect fact that balance is no longer stored in Account class

--- lib/account.rb
+++ lib/account.rb
@@ -5,14 +5,13 @@ class Account
   attr_reader :balance, :transaction
 
   def initialize(transaction = Transaction.new)
-  @balance = 0
   @transaction = transaction
   end
 
   def add_payment(amount)
     @transaction.payment(amount)
-    @balance += amount
+    amount
   end
 
-  
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,26 +4,24 @@ describe Account do
   subject(:account) {described_class.new}
   let(:transactions) {double :transactions}
 
-  it 'is instantiated with a balance of 0' do
-    expect(account.balance).to eq(0)
+  it 'is instantiated with a transaction object' do
+    expect(account.transaction).to be_instance_of(Transaction)
   end
 
   describe '#payment' do
 
-    it 'increases the account balance if the payment amount is positive' do
+    it 'returns the value of the transaction after execution' do
+      expect(account.add_payment(10)).to eq 10
+    end
+
+    it 'adds the amount of the payment to the transaction record' do
       account.add_payment(10)
-      expect(account.balance).to eq(10)
+      expect(account.transaction.transactions[0][:amount]).to eq 10
     end
 
-    it 'decreases the account balance if the payment amount is negative' do
-      account.add_payment(-10)
-      expect(account.balance).to eq(-10)
+    it 'adds the current account balance to the transaction record' do
+      account.add_payment(10)
+      expect(account.transaction.transactions[0][:balance]).to eq 10
     end
   end
-
-  it 'adds the current account balance to the transaction record' do
-    account.add_payment(10)
-    expect(account.transaction.transactions[0][:balance]).to eq 10
-  end
-
 end
